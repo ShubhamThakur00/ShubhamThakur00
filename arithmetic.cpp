afce230f7bdd122abdfd5aec054082b2a18b3aba
ShubhamThakur00 <115650921+ShubhamThakur00@users.noreply.github.com>
2022-10-12 21:38:30 +0530

    Create arithmetic.cpp
    successful run

---
+++ arithmetic.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 15, c = 20;
+    cout << "addition:-";
+    cout << a + b;
+
+    cout << endl;
+
+    cout << "subtaction:-";
+    cout << c - b;
+
+cout<<endl;
+
+cout<<"multiplie:-";
+cout<<a*b;
+
+cout<<endl;
+
+cout<<"divide:-";
+cout<<a/c;
+
+cout<<endl;
+
+cout<<"module:-";
+cout<<c%a;
+
+    return 0;
+}
